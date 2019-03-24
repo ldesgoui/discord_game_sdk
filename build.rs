1f9be2ed7931c90a759995594b405231b42fab5d
ldesgoui <ldesgoui@gmail.com>
2019-03-24 19:04:15 +0100

    un-hardcode path (soften ..?)

--- build.rs
+++ build.rs
@@ -1,4 +1,4 @@
 fn main() {
     println!("cargo:rustc-link-lib=discord_game_sdk");
-    println!("cargo:rustc-link-search=/home/ldesgoui/discord-game-sdk/target/debug");
+    println!("cargo:rustc-link-search=./target/debug");
 }
