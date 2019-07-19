85276936667872699e72fb2c9d85b4f9c411f95e
ldesgoui <ldesgoui@gmail.com>
2019-07-19 18:21:04 +0200

    example

---
+++ examples/simple.rs
@@ -0,0 +1,19 @@
+use discord_game_sdk::*;
+
+fn main() {
+    pretty_env_logger::init();
+
+    let client_id = 0;
+    let mut gsdk = Discord::new(client_id).unwrap();
+
+    // Game loop
+    loop {
+        gsdk.empty_event_receivers();
+        if let Err(e) = gsdk.run_callbacks() {
+            log::info!("run_callbacks error: {}", e);
+            return;
+        }
+
+        std::thread::sleep(std::time::Duration::from_millis(16));
+    }
+}
