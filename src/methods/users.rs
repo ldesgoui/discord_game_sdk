c9f5240aac4b83d2f9204858ca24a53957bbe36d
ldesgoui <ldesgoui@ldesgoui.xyz>
2020-01-05 00:39:26 +0100

    documenting user methods

--- src/methods/users.rs
+++ src/methods/users.rs
@@ -8,11 +8,23 @@ use crate::{
 /// > [Chapter in official docs](https://discordapp.com/developers/docs/game-sdk/users)
 impl<'a> Discord<'a> {
     /// Get the current user.
+    ///
     /// More information can be found through the HTTP API.
     ///
-    /// Will return `Err(_)` until [`event::user::CurrentUserUpdate`](struct.CurrentUserUpdate.html).
+    /// ## Errors
+    ///
+    /// Until the event [`CurrentUserUpdate`] is fired, this method will return an error.
     ///
     /// > [Method in official docs](https://discordapp.com/developers/docs/game-sdk/users#getcurrentuser)
+    ///
+    /// [`CurrentUserUpdate`]: event/struct.CurrentUserUpdate.html
+    ///
+    /// ```rust
+    /// # use discord_game_sdk::*;
+    /// # fn example(discord: Discord) -> Result<()> {
+    /// let current_user = discord.current_user()?;
+    /// # Ok(()) }
+    /// ```
     pub fn current_user(&self) -> Result<User> {
         let mut user = User(sys::DiscordUser::default());
 
@@ -24,6 +36,21 @@ impl<'a> Discord<'a> {
     /// Get user information for a given ID.
     ///
     /// > [Method in official docs](https://discordapp.com/developers/docs/game-sdk/users#getuser)
+    ///
+    /// ```rust
+    /// # use discord_game_sdk::*;
+    /// # fn example(discord: Discord) -> Result<()> {
+    /// # let id_to_lookup = 0;
+    /// discord.user(id_to_lookup, |discord, result| {
+    ///     match result {
+    ///         Ok(user) => {
+    ///             // ...
+    ///         },
+    ///         Err(error) => eprintln!("failed to fetch user: {}", error),
+    ///     }
+    /// });
+    /// # Ok(()) }
+    /// ```
     pub fn user(&self, user_id: i64, callback: impl 'a + FnMut(&Discord<'_>, Result<User>)) {
         unsafe {
             ffi!(self
@@ -36,6 +63,13 @@ impl<'a> Discord<'a> {
     /// Get the Premium type for the currently connected user.
     ///
     /// > [Method in official docs](https://discordapp.com/developers/docs/game-sdk/users#getcurrentuserpremiumtype)
+    ///
+    /// ```rust
+    /// # use discord_game_sdk::*;
+    /// # fn example(discord: Discord) -> Result<()> {
+    /// let premium = discord.current_user_premium_kind()?;
+    /// # Ok(()) }
+    /// ```
     pub fn current_user_premium_kind(&self) -> Result<PremiumKind> {
         let mut premium_type = sys::EDiscordPremiumType::default();
 
@@ -52,6 +86,13 @@ impl<'a> Discord<'a> {
     /// Return a bitfield of all flags set for the current user.
     ///
     /// > [Method in official docs](https://discordapp.com/developers/docs/game-sdk/users#currentuserhasflag)
+    ///
+    /// ```rust
+    /// # use discord_game_sdk::*;
+    /// # fn example(discord: Discord) -> Result<()> {
+    /// let flags = discord.current_user_flags()?;
+    /// # Ok(()) }
+    /// ```
     pub fn current_user_flags(&self) -> Result<UserFlags> {
         let mut flags = UserFlags::empty();
 
@@ -80,6 +121,16 @@ impl<'a> Discord<'a> {
     /// Fires when the User struct of the currently connected user changes.
     ///
     /// > [Method in official docs](https://discordapp.com/developers/docs/game-sdk/users#oncurrentuserupdate)
+    ///
+    /// ```rust
+    /// # use discord_game_sdk::*;
+    /// # fn example(discord: Discord) -> Result<()> {
+    /// # let mut can_get_current_user = false;
+    /// if discord.recv_current_user_update().count() > 0 {
+    ///     can_get_current_user = true;
+    /// }
+    /// # Ok(()) }
+    /// ```
     pub fn recv_current_user_update(&self) -> impl '_ + Iterator<Item = event::CurrentUserUpdate> {
         self.receivers.current_user_update.try_iter()
     }
