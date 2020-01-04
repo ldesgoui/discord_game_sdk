cd3c2c336d4c85b8c81e8e9553dcd533e96eb936
ldesgoui <ldesgoui@ldesgoui.xyz>
2020-01-04 23:33:27 +0100

    docs

--- src/methods/applications.rs
+++ src/methods/applications.rs
@@ -10,11 +10,17 @@ use std::mem::size_of;
 ///
 /// Authentication and various helper functions
 ///
-/// <https://discordapp.com/developers/docs/game-sdk/applications>
+/// > [Chapter in official docs](https://discordapp.com/developers/docs/game-sdk/applications)
 impl<'a> Discord<'a> {
     /// The locale that was set by the current user in their Discord settings.
     ///
-    /// <https://discordapp.com/developers/docs/game-sdk/applications#getcurrentlocale>
+    /// > [Method in official docs](https://discordapp.com/developers/docs/game-sdk/applications#getcurrentlocale)
+    ///
+    /// ```rust
+    /// # use discord_game_sdk::*;
+    /// # fn example(discord: Discord) -> Result<()> {
+    /// println!("current locale is {}", discord.current_locale());
+    /// # Ok(()) }
     pub fn current_locale(&self) -> String {
         let mut locale: sys::DiscordLocale = [0; size_of::<sys::DiscordLocale>()];
 
@@ -28,10 +34,17 @@ impl<'a> Discord<'a> {
     }
 
     /// Get the name of pushed branch on which the game is running.
+    ///
     /// These are branches that you created and pushed using
     /// [Dispatch](https://discordapp.com/developers/docs/dispatch/dispatch-and-you).
     ///
-    /// <https://discordapp.com/developers/docs/game-sdk/applications#getcurrentbranch>
+    /// > [Method in official docs](https://discordapp.com/developers/docs/game-sdk/applications#getcurrentbranch)
+    ///
+    /// ```rust
+    /// # use discord_game_sdk::*;
+    /// # fn example(discord: Discord) -> Result<()> {
+    /// println!("current branch is {}", discord.current_branch());
+    /// # Ok(()) }
     pub fn current_branch(&self) -> String {
         let mut branch: sys::DiscordBranch = [0; size_of::<sys::DiscordBranch>()];
 
@@ -46,7 +59,15 @@ impl<'a> Discord<'a> {
 
     /// Checks if the current user has the entitlement to run this game.
     ///
-    /// <https://discordapp.com/developers/docs/game-sdk/applications#validateorexit>
+    /// > [Method in official docs](https://discordapp.com/developers/docs/game-sdk/applications#validateorexit)
+    ///
+    /// ```rust
+    /// # use discord_game_sdk::*;
+    /// # fn example(discord: Discord) -> Result<()> {
+    /// discord.validate_or_exit(|discord, result| {
+    ///     // ...
+    /// });
+    /// # Ok(()) }
     pub fn validate_or_exit(&self, callback: impl 'a + FnMut(&Discord<'_>, Result<()>)) {
         unsafe {
             ffi!(self
@@ -57,12 +78,26 @@ impl<'a> Discord<'a> {
     }
 
     /// Retrieve an OAuth 2.0 Bearer token for the current user.
+    ///
     /// If your game was launched from Discord and you call this function,
     /// you will automatically receive the token.
     /// If the game was not launched from Discord and this method is called,
     /// Discord will focus itself and prompt the user for authorization.
     ///
-    /// <https://discordapp.com/developers/docs/game-sdk/applications#getoauth2token>
+    /// > [Method in official docs](https://discordapp.com/developers/docs/game-sdk/applications#getoauth2token)
+    ///
+    /// ```rust
+    /// # use discord_game_sdk::*;
+    /// # fn example(discord: Discord) -> Result<()> {
+    /// discord.oauth_token(|discord, token| {
+    ///     match token {
+    ///         Ok(token) => {
+    ///             //...
+    ///         },
+    ///         Err(error) => eprintln!("failed to retrieve OAuth2 token: {}", error);
+    ///     }
+    /// });
+    /// # Ok(()) }
     pub fn oauth2_token(&self, callback: impl 'a + FnMut(&Discord<'_>, Result<OAuth2Token>)) {
         unsafe {
             ffi!(self
@@ -74,7 +109,20 @@ impl<'a> Discord<'a> {
 
     /// Get the signed app ticket for the current user.
     ///
-    /// <https://discordapp.com/developers/docs/game-sdk/applications#getticket>
+    /// > [Method in official docs](https://discordapp.com/developers/docs/game-sdk/applications#getticket)
+    ///
+    /// ```rust
+    /// # use discord_game_sdk::*;
+    /// # fn example(discord: Discord) -> Result<()> {
+    /// discord.app_ticket(|discord, ticket| {
+    ///     match ticket {
+    ///         Ok(ticket) => {
+    ///             //...
+    ///         },
+    ///         Err(error) => eprintln!("failed to retrieve signed app ticket: {}", error);
+    ///     }
+    /// });
+    /// # Ok(()) }
     pub fn app_ticket(&self, callback: impl 'a + FnMut(&Discord<'_>, Result<String>)) {
         unsafe {
             ffi!(self
