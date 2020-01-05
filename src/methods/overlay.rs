aa3d5d8a343c4c9094a4b21fc0b9fcae8bbdc9e0
ldesgoui <ldesgoui@ldesgoui.xyz>
2020-01-05 01:11:20 +0100

    doc overlay

--- src/methods/overlay.rs
+++ src/methods/overlay.rs
@@ -14,10 +14,20 @@ use std::borrow::Cow;
 /// > [Chapter in official docs](https://discordapp.com/developers/docs/game-sdk/overlay)
 impl<'a> Discord<'a> {
     /// Check whether the user has the overlay enabled or disabled.
+    ///
     /// If the overlay is disabled, all the functionality in this manager will still work.
     /// The calls will instead focus the Discord client and show the modal there instead.
     ///
     /// > [Method in official docs](https://discordapp.com/developers/docs/game-sdk/overlay#isenabled)
+    ///
+    /// ```rust
+    /// # use discord_game_sdk::*;
+    /// # fn example(discord: Discord) -> Result<()> {
+    /// if discord.overlay_enabled() {
+    ///     // ...
+    /// }
+    /// # Ok(()) }
+    /// ```
     pub fn overlay_enabled(&self) -> bool {
         let mut enabled = false;
 
@@ -29,6 +39,15 @@ impl<'a> Discord<'a> {
     /// Whether the overlay is appearing and has taken focus.
     ///
     /// > [Method in official docs](https://discordapp.com/developers/docs/game-sdk/overlay#islocked)
+    ///
+    /// ```rust
+    /// # use discord_game_sdk::*;
+    /// # fn example(discord: Discord) -> Result<()> {
+    /// if discord.overlay_opened() {
+    ///     // ...
+    /// }
+    /// # Ok(()) }
+    /// ```
     pub fn overlay_opened(&self) -> bool {
         let mut locked = false;
 
@@ -40,6 +59,17 @@ impl<'a> Discord<'a> {
     /// Open or close the overlay.
     ///
     /// > [Method in official docs](https://discordapp.com/developers/docs/game-sdk/overlay#setlocked)
+    ///
+    /// ```rust
+    /// # use discord_game_sdk::*;
+    /// # fn example(discord: Discord) -> Result<()> {
+    /// discord.set_overlay_opened(false, |discord, result| {
+    ///     if let Err(error) = result {
+    ///         return eprintln!("failed to set overlay open: {}", error);
+    ///     }
+    /// });
+    /// # Ok(()) }
+    /// ```
     pub fn set_overlay_opened(
         &self,
         opened: bool,
@@ -57,6 +87,17 @@ impl<'a> Discord<'a> {
     /// If you do not have a valid activity with all the required fields, this call will error.
     ///
     /// > [Method in official docs](https://discordapp.com/developers/docs/game-sdk/overlay#openactivityinvite)
+    ///
+    /// ```rust
+    /// # use discord_game_sdk::*;
+    /// # fn example(discord: Discord) -> Result<()> {
+    /// discord.open_invite_overlay(Action::Join, |discord, result| {
+    ///     if let Err(error) = result {
+    ///         return eprintln!("failed open invite overlay: {}", error);
+    ///     }
+    /// });
+    /// # Ok(()) }
+    /// ```
     pub fn open_invite_overlay(
         &self,
         action: Action,
@@ -75,9 +116,22 @@ impl<'a> Discord<'a> {
     ///
     /// Receiving `Ok(())` does not necessarily mean that the user has joined the guild.
     ///
-    /// A nul byte will be appended to `code` if necessary.
+    /// ## Performance
+    ///
+    /// A nul byte will be appended to `code` if one is not present.
     ///
     /// > [Method in official docs](https://discordapp.com/developers/docs/game-sdk/overlay#openguildinvite)
+    ///
+    /// ```rust
+    /// # use discord_game_sdk::*;
+    /// # fn example(discord: Discord) -> Result<()> {
+    /// discord.open_guild_invite_overlay("discord-gamesdk\0", |discord, result| {
+    ///     if let Err(error) = result {
+    ///         return eprintln!("failed open guild invite overlay: {}", error);
+    ///     }
+    /// });
+    /// # Ok(()) }
+    /// ```
     pub fn open_guild_invite_overlay<'b>(
         &self,
         code: impl Into<Cow<'b, str>>,
@@ -102,6 +156,17 @@ impl<'a> Discord<'a> {
     /// That means that a user can have different favorite voice settings for each of their games.
     ///
     /// > [Method in official docs](https://discordapp.com/developers/docs/game-sdk/overlay#openvoicesettings)
+    ///
+    /// ```rust
+    /// # use discord_game_sdk::*;
+    /// # fn example(discord: Discord) -> Result<()> {
+    /// discord.open_voice_settings(|discord, result| {
+    ///     if let Err(error) = result {
+    ///         return eprintln!("failed open voice settings overlay: {}", error);
+    ///     }
+    /// });
+    /// # Ok(()) }
+    /// ```
     pub fn open_voice_settings(&self, callback: impl 'a + FnMut(&Discord<'_>, Result<()>)) {
         unsafe {
             ffi!(self
@@ -114,6 +179,18 @@ impl<'a> Discord<'a> {
     /// Fires when the overlay is opened or closed.
     ///
     /// > [Method in official docs](https://discordapp.com/developers/docs/game-sdk/overlay#ontoggle)
+    ///
+    /// ```rust
+    /// # use discord_game_sdk::*;
+    /// # fn example(discord: Discord) -> Result<()> {
+    /// for toggle in discord.recv_overlay_toggle() {
+    ///     println!(
+    ///         "overlay is {}",
+    ///         if toggle.closed { "opened" } else { "closed" }
+    ///     );
+    /// }
+    /// # Ok(()) }
+    /// ```
     pub fn recv_overlay_toggle(&self) -> impl '_ + Iterator<Item = event::OverlayToggle> {
         self.receivers.overlay_toggle.try_iter()
     }
