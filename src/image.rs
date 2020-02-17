4503e9f7b130bff7c4c1be2fc0664cfc24329828
Lucas Desgouilles <ldesgoui@ldesgoui.xyz>
2020-02-17 20:53:27 +0100

    breaking change: remove Image::pixel (bad API) (#86)

--- src/image.rs
+++ src/image.rs
@@ -30,21 +30,6 @@ impl Image {
     pub fn data(&self) -> &[u8] {
         &self.data
     }
-
-    /// Get R, G, B, A channels of pixel at (x, y)
-    pub fn pixel(&self, x: u32, y: u32) -> (u8, u8, u8, u8) {
-        debug_assert!(x < self.width);
-        debug_assert!(y < self.height);
-
-        let idx = x as usize + y as usize * self.width as usize;
-
-        (
-            self.data[idx],
-            self.data[idx + 1],
-            self.data[idx + 2],
-            self.data[idx + 3],
-        )
-    }
 }
 
 #[cfg(feature = "image")]
