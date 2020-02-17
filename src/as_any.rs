ldesgoui/discord_game_sdk

commit 1ca88e943ce81ac19ad75271f731999d9eaedefd

use UnsafeCell to make macros legal (#79)

src/discord.rs
@@ -1,4 +1,5 @@
 use crate::{sys, EventHandler};
+use std::cell::UnsafeCell;
 
 /// Main interface with SDK
 ///
@@ -25,7 +26,17 @@ use crate::{sys, EventHandler};
 /// - [Voice](#voice)
 #[derive(Debug)]
 #[repr(transparent)]
-pub struct Discord(pub(crate) Box<DiscordInner>);
+pub struct Discord(pub(crate) UnsafeCell<DiscordInner>);
+
+impl Discord {
+    pub(crate) fn inner(&self) -> &DiscordInner {
+        unsafe { &*self.0.get() }
+    }
+
+    pub(crate) fn inner_mut(&mut self) -> &mut DiscordInner {
+        unsafe { &mut *self.0.get() }
+    }
+}
 
 impl Drop for Discord {
     fn drop(&mut self) {

src/macros.rs
@@ -1,16 +1,19 @@
+// Don't trust me
+
 macro_rules! ffi {
     // ffi!(self.destroy())
     ($self:ident.$method:ident($($arg:expr),* $(,)?)) => {{
-        debug_assert!(!$self.0.core.is_null());
+        let core = $self.inner().core;
+        debug_assert!(!core.is_null());
 
         log::trace!("FFI: {}", stringify!($method));
-        let function = (*$self.0.core).$method;
+        let function = (*core).$method;
 
         debug_assert!(function.is_some());
 
         let function = function.unwrap();
 
-        function($self.0.core, $($arg),*)
+        function(core, $($arg),*)
     }};
 
     // ffi!(self.get_application_manager().get_current_locale())
@@ -41,7 +44,6 @@ macro_rules! ffi {
         })
     ) => {{
         use crate::utils::CallbackData;
-        use std::ops::Deref;
 
         let manager = ffi!($self.$get_manager());
 
@@ -65,24 +67,17 @@ macro_rules! ffi {
             // SAFETY: Legal, we created the pointer just below
             let callback_data = Box::from_raw(callback_data as *mut CallbackData<$res>);
 
-            // SAFETY: Legal (Mutable aliasing)
-            // - We don't mutate the Box<DiscordInner> (no dropping either)
-            // - We only pass a &Discord
-            // - At this point in time, we are *always* in Discord::run_callbacks
-            // - Which means we're in posession of &mut Discord
-            // - And downgrading to a &Discord and passing it to another function is fine
-            let discord = Discord(Box::from_raw(callback_data.discord as *mut _));
+            // SAFETY: Legal, repr(transparent) means
+            // *const Discord == *const UnsafeCell<DiscordInner> == *const DiscordInner
+            let discord = &*(callback_data.discord as *const Discord);
 
-            (callback_data.callback)(&discord, $expr);
-
-            // SAFETY: We cannot drop Box<DiscordInner>
-            std::mem::forget(discord);
+            (callback_data.callback)(discord, $expr);
         }
 
         let callback = Box::new($callback);
 
         let callback_data = CallbackData {
-            discord: $self.0.deref() as *const _,
+            discord: $self.0.get(),
             callback,
         };
 
@@ -125,36 +120,32 @@ macro_rules! event_handler {
             inner: *mut std::ffi::c_void,
             $($param: $ty),*
         ) {
-            use crate::discord::{Discord, DiscordInner};
+            use crate::discord::Discord;
 
             prevent_unwind!();
 
             debug_assert!(!inner.is_null());
 
-            // SAFETY: Legal (Mutable aliasing)
-            // - We don't drop the Box<DiscordInner>
-            // - At this point in time, we are *always* in Discord::run_callbacks
-            // - Which means we're in posession of &mut Discord
-            // - We are absolutely sure that only one of the &mut are used at one time
-            let mut discord = Discord(Box::from_raw(inner as *mut DiscordInner));
+            // SAFETY: Legal, repr(transparent) means
+            // *const Discord == *const UnsafeCell<DiscordInner> == *const DiscordInner
+            let discord = &*(inner as *const Discord);
 
-            // We're sure that the event handler won't be used through discord,
-            // we replace it with a blank value for now
-            let mut event_handler = discord.0.event_handler.take();
+            // SAFETY: Legal, writing to `*mut DiscordInner` while `&mut DiscordInner` is in our callstack
+            // We're also sure that `DiscordInner::event_handler` cannot be used during the event
+            // handling method call, grabbing ownership and replacing it with None for the time
+            // being is fine
+            let mut event_handler = (*discord.0.get()).event_handler.take();
 
             if let Some(event_handler) = event_handler.as_mut() {
                 event_handler.$method(
-                    &discord,
+                    discord,
                     $($expr),*
                 );
             }
 
             // We're sure this will go through, otherwise we're panicking
             // and therefore about to abort
-            discord.0.event_handler = event_handler;
-
-            // SAFETY: We cannot drop Box<DiscordInner>
-            std::mem::forget(discord);
+            (*discord.0.get()).event_handler = event_handler;
         }
 
         Some($method)

src/methods/core.rs
@@ -6,7 +6,7 @@ use crate::{
     Activity, ClientID, CreateFlags, Entitlement, EventHandler, Relationship, Result, User,
     UserAchievement,
 };
-use std::{convert::TryFrom, ops::Deref};
+use std::{cell::UnsafeCell, convert::TryFrom};
 
 /// # Core
 ///
@@ -46,14 +46,18 @@ impl Discord {
     /// > [`SetLogHook` in official docs](https://discordapp.com/developers/docs/game-sdk/discord#setloghook)
     #[allow(clippy::cognitive_complexity)]
     pub fn with_create_flags(client_id: ClientID, flags: CreateFlags) -> Result<Self> {
-        let mut inner = Box::new(DiscordInner {
+        let inner = UnsafeCell::new(DiscordInner {
+            // SAFETY: this is written to during `sys::DiscordCreate`
             core: std::ptr::null_mut(),
             client_id,
             event_handler: None,
         });
 
-        // SAFETY: This is the pointer we use in event handler code
-        let ptr = inner.deref() as *const DiscordInner as *mut std::ffi::c_void;
+        // SAFETY: This is the pointer we use in event handler code, repr(transparent) means
+        // *const Discord == *const Unsafecell<DiscordInner> == *const DiscordInner
+        let ptr = &inner as *const UnsafeCell<DiscordInner> as *mut std::ffi::c_void;
+
+        log::trace!("instantiating with client ID {}", client_id);
 
         let params = create_params(client_id, flags.into(), ptr);
 
@@ -63,19 +67,20 @@ impl Discord {
                 // XXX: *mut should be *const
                 &params as *const _ as *mut _,
                 // XXX: *mut *mut should be *mut *const
-                &mut inner.core,
+                &mut (*inner.get()).core,
             )
+            .to_result()?;
         }
-        .to_result()?;
-
-        log::trace!("received pointer to {:p}", inner.core);
 
         let instance = Discord(inner);
+
+        log::trace!("received pointer to {:p}", instance.inner().core);
 
         #[allow(unused_results)]
         unsafe {
             ffi!(instance.set_log_hook(
                 sys::DiscordLogLevel_Debug,
+                // SAFETY: this is never used
                 std::ptr::null_mut(),
                 Some(log_hook),
             ));
@@ -115,7 +120,7 @@ impl Discord {
 
     /// The Client ID that was supplied during creation
     pub fn client_id(&self) -> ClientID {
-        self.0.client_id
+        self.inner().client_id
     }
 
     /// Replaces the current event handler
@@ -123,18 +128,21 @@ impl Discord {
         &mut self,
         event_handler: Box<dyn EventHandler>,
     ) -> Option<Box<dyn EventHandler>> {
-        self.0.event_handler.replace(event_handler)
+        self.inner_mut().event_handler.replace(event_handler)
     }
 
     /// Takes the current event handler, leaving `None` in its place
     pub fn take_event_handler(&mut self) -> Option<Box<dyn EventHandler>> {
-        self.0.event_handler.take()
+        self.inner_mut().event_handler.take()
     }
 
     /// Returns some mutable reference to the event handler if it is of type T, or None if it isn't.
     // We require &mut self to prevent calling during callbacks
     pub fn downcast_event_handler<T: std::any::Any>(&mut self) -> Option<&mut T> {
-        self.0.event_handler.as_mut().and_then(|e| e.downcast_mut())
+        self.inner_mut()
+            .event_handler
+            .as_mut()
+            .and_then(|e| e.downcast_mut())
     }
 }
 

src/utils.rs
@@ -5,13 +5,19 @@ pub(crate) struct CallbackData<'d, T> {
     pub(crate) callback: Box<dyn 'd + FnOnce(&Discord, T)>,
 }
 
+// Literal hack to be able to use `ffi!(...)` with `LobbyTransaction`, `LobbyMemberTransaction`,
+// and `SearchQuery`.
 pub(crate) struct MacroHelper<T> {
     pub(crate) core: *mut T,
 }
 
 impl<T> MacroHelper<T> {
-    pub(crate) fn new(core: *mut T) -> (Self,) {
-        (Self { core },)
+    pub(crate) fn new(core: *mut T) -> Self {
+        Self { core }
+    }
+
+    pub(crate) fn inner(&self) -> &Self {
+        self
     }
 }
 

commit 1e705c0fc7b07c617bee4a8d77b56fe0663e786b

Fix insane breakage introduced in #79 (#83)

src/as_any.rs
@@ -1,19 +1,0 @@
-// Thanks to Globi for the help
-// This is referred to as "upcasting"
-
-use std::any::Any;
-
-pub trait AsAny {
-    fn as_any(&self) -> &dyn Any;
-    fn as_any_mut(&mut self) -> &mut dyn Any;
-}
-
-impl<T: Any> AsAny for T {
-    fn as_any(&self) -> &dyn Any {
-        self
-    }
-
-    fn as_any_mut(&mut self) -> &mut dyn Any {
-        self
-    }
-}

src/discord.rs
@@ -1,4 +1,4 @@
-use crate::{sys, EventHandler};
+use crate::{sys, utils::MacroHelper, EventHandler};
 use std::cell::UnsafeCell;
 
 /// Main interface with SDK
@@ -25,29 +25,32 @@ use std::cell::UnsafeCell;
 /// - [Users](#users)
 /// - [Voice](#voice)
 #[derive(Debug)]
-#[repr(transparent)]
-pub struct Discord(pub(crate) UnsafeCell<DiscordInner>);
+pub struct Discord(pub(crate) Box<DiscordInner>);
 
 impl Discord {
     pub(crate) fn inner(&self) -> &DiscordInner {
-        unsafe { &*self.0.get() }
-    }
-
-    pub(crate) fn inner_mut(&mut self) -> &mut DiscordInner {
-        unsafe { &mut *self.0.get() }
-    }
-}
-
-impl Drop for Discord {
-    fn drop(&mut self) {
-        unsafe { ffi!(self.destroy()) }
+        &*self.0
     }
 }
 
 pub(crate) struct DiscordInner {
     pub(crate) core: *mut sys::IDiscordCore,
     pub(crate) client_id: sys::DiscordClientId,
-    pub(crate) event_handler: Option<Box<dyn EventHandler>>,
+    pub(crate) event_handler: UnsafeCell<Option<Box<dyn EventHandler>>>,
+}
+
+impl DiscordInner {
+    pub(crate) fn event_handler_mut(&mut self) -> &mut Option<Box<dyn EventHandler>> {
+        unsafe { &mut *self.event_handler.get() }
+    }
+}
+
+impl Drop for DiscordInner {
+    fn drop(&mut self) {
+        let core = MacroHelper::new(self.core);
+
+        unsafe { ffi!(core.destroy()) }
+    }
 }
 
 impl std::fmt::Debug for DiscordInner {

src/event_handler.rs
@@ -1,6 +1,6 @@
 use crate::{
-    as_any::AsAny, Action, Activity, Discord, Entitlement, LobbyID, NetworkChannelID,
-    NetworkPeerID, Relationship, User, UserAchievement, UserID,
+    utils::AsAny, Action, Activity, Discord, Entitlement, LobbyID, NetworkChannelID, NetworkPeerID,
+    Relationship, User, UserAchievement, UserID,
 };
 use std::any::Any;
 

src/lib.rs
@@ -120,7 +120,6 @@ mod action;
 mod activity;
 mod activity_kind;
 mod aliases;
-mod as_any;
 mod cast;
 mod comparison;
 mod create_flags;

src/macros.rs
@@ -8,9 +8,7 @@ macro_rules! ffi {
 
         log::trace!("FFI: {}", stringify!($method));
         let function = (*core).$method;
-
         debug_assert!(function.is_some());
-
         let function = function.unwrap();
 
         function(core, $($arg),*)
@@ -22,14 +20,11 @@ macro_rules! ffi {
         .$method:ident($($arg:expr),* $(,)?)
     ) => {{
         let manager = ffi!($self.$get_manager());
-
         debug_assert!(!manager.is_null());
 
         log::trace!("FFI: .{}", stringify!($method));
         let function = (*manager).$method;
-
         debug_assert!(function.is_some());
-
         let function = function.unwrap();
 
         function(manager, $($arg),*)
@@ -43,17 +38,14 @@ macro_rules! ffi {
             $callback:ident::<$res:ty>($expr:expr $(,)?)
         })
     ) => {{
-        use crate::utils::CallbackData;
+        use crate::{discord::DiscordInner, utils::CallbackData};
 
         let manager = ffi!($self.$get_manager());
-
         debug_assert!(!manager.is_null());
 
         log::trace!("FFI: .{}", stringify!($method));
         let function = (*manager).$method;
-
         debug_assert!(function.is_some());
-
         let function = function.unwrap();
 
         unsafe extern "C" fn c_fn(
@@ -64,20 +56,27 @@ macro_rules! ffi {
 
             debug_assert!(!callback_data.is_null());
 
-            // SAFETY: Legal, we created the pointer just below
+            // SAFETY: We created the pointer just below
             let callback_data = Box::from_raw(callback_data as *mut CallbackData<$res>);
 
-            // SAFETY: Legal, repr(transparent) means
-            // *const Discord == *const UnsafeCell<DiscordInner> == *const DiscordInner
-            let discord = &*(callback_data.discord as *const Discord);
+            // SAFETY:
+            // We're duplicating the `Box<DiscordInner>`, this is safe:
+            // - We're not mutating it, we're not dropping it
+            // - No other part of the code will mutate it as `&mut Discord` is in the callstack
+            let discord = Discord(Box::from_raw(callback_data.discord));
 
-            (callback_data.callback)(discord, $expr);
+            (callback_data.callback)(&discord, $expr);
+
+            // SAFETY: Not dropping our duplicated `Box<DiscordInner>`
+            std::mem::forget(discord);
         }
 
         let callback = Box::new($callback);
 
         let callback_data = CallbackData {
-            discord: $self.0.get(),
+            // SAFETY: We're passing the Box as raw pointer to duplicate it above
+            // It won't be mutated or dropped
+            discord: &*$self.0 as *const DiscordInner as *mut DiscordInner,
             callback,
         };
 
@@ -120,32 +119,35 @@ macro_rules! event_handler {
             inner: *mut std::ffi::c_void,
             $($param: $ty),*
         ) {
-            use crate::discord::Discord;
+            use crate::discord::{Discord, DiscordInner};
 
             prevent_unwind!();
 
             debug_assert!(!inner.is_null());
 
-            // SAFETY: Legal, repr(transparent) means
-            // *const Discord == *const UnsafeCell<DiscordInner> == *const DiscordInner
-            let discord = &*(inner as *const Discord);
+            // SAFETY:
+            // We're duplicating the `Box<DiscordInner>`, this is safe:
+            // - We're not mutating it, we're not dropping it
+            // - No other part of the code will mutate it as `&mut Discord` is in the callstack
+            let discord = Discord(Box::from_raw(inner as *mut DiscordInner));
 
-            // SAFETY: Legal, writing to `*mut DiscordInner` while `&mut DiscordInner` is in our callstack
-            // We're also sure that `DiscordInner::event_handler` cannot be used during the event
-            // handling method call, grabbing ownership and replacing it with None for the time
-            // being is fine
-            let mut event_handler = (*discord.0.get()).event_handler.take();
+            // SAFETY: Mutation through immutable reference
+            // - `discord.0.event_handler` is an `UnsafeCell`, inner mutation is legal
+            // - No other part of the code can safely mutate it as they require `&mut DiscordInner`
+            // - `EventHandler` can mutate itself during method but not `&Discord`
+            let mut event_handler = (*discord.0.event_handler.get()).take();
 
             if let Some(event_handler) = event_handler.as_mut() {
                 event_handler.$method(
-                    discord,
+                    &discord,
                     $($expr),*
                 );
             }
 
-            // We're sure this will go through, otherwise we're panicking
-            // and therefore about to abort
-            (*discord.0.get()).event_handler = event_handler;
+            (*discord.0.event_handler.get()) = event_handler;
+
+            // SAFETY: Not dropping our duplicated `Box<DiscordInner>`
+            std::mem::forget(discord);
         }
 
         Some($method)

src/methods/core.rs
@@ -44,61 +44,76 @@ impl Discord {
     ///
     /// > [`Create` in official docs](https://discordapp.com/developers/docs/game-sdk/discord#create)  
     /// > [`SetLogHook` in official docs](https://discordapp.com/developers/docs/game-sdk/discord#setloghook)
-    #[allow(clippy::cognitive_complexity)]
     pub fn with_create_flags(client_id: ClientID, flags: CreateFlags) -> Result<Self> {
-        let inner = UnsafeCell::new(DiscordInner {
-            // SAFETY: this is written to during `sys::DiscordCreate`
+        // This is a mess
+        //
+        // - We want to call `sys::DiscordCreate`, it gives us a `*mut sys::IDiscordCore`
+        // - We provide `&mut EventHandler` and `&Discord` during event handlers
+        // - That means we need to mutate `DiscordInner::event_handler`, which is fine via `UnsafeCell`
+        // - That also means we need to pass the `Box` as raw pointer to duplicate it
+        // - `sys::DiscordCreate` wants `sys::DiscordCreateParams` + `*mut *mut sys::IDiscordCore`
+        // - `sys::DiscordCreateParams` wants our `event_data: *mut c_void`
+        // - Our `event_data` is the raw `Box` pointer
+        // - We need to build the `Box<DiscordInner>` first to pass a valid pointer
+
+        log::debug!("instantiating with client ID {}", client_id);
+
+        let mut instance = Discord(Box::new(DiscordInner {
+            // SAFETY: overwritten by `sys::DiscordCreate`, not deref'd until then
             core: std::ptr::null_mut(),
             client_id,
-            event_handler: None,
-        });
+            event_handler: UnsafeCell::new(None),
+        }));
 
-        // SAFETY: This is the pointer we use in event handler code, repr(transparent) means
-        // *const Discord == *const Unsafecell<DiscordInner> == *const DiscordInner
-        let ptr = &inner as *const UnsafeCell<DiscordInner> as *mut std::ffi::c_void;
-
-        log::trace!("instantiating with client ID {}", client_id);
-
-        let params = create_params(client_id, flags.into(), ptr);
+        // SAFETY: As described above, we're passing the Box as raw pointer
+        // It'll be used to duplicate the `Box` but won't mutate or drop it
+        let ptr = &mut *instance.0 as *mut DiscordInner as *mut std::ffi::c_void;
+        let mut params = create_params(client_id, flags.into(), ptr);
 
         unsafe {
             sys::DiscordCreate(
                 sys::DISCORD_VERSION,
                 // XXX: *mut should be *const
-                &params as *const _ as *mut _,
-                // XXX: *mut *mut should be *mut *const
-                &mut (*inner.get()).core,
+                &mut params,
+                &mut instance.0.core,
             )
             .to_result()?;
         }
 
-        let instance = Discord(inner);
+        log::trace!("received pointer to {:p}", instance.0.core);
 
-        log::trace!("received pointer to {:p}", instance.inner().core);
+        instance.set_log_hook();
+        instance.kickstart_managers();
 
-        #[allow(unused_results)]
+        Ok(instance)
+    }
+
+    fn set_log_hook(&self) {
         unsafe {
-            ffi!(instance.set_log_hook(
+            ffi!(self.set_log_hook(
                 sys::DiscordLogLevel_Debug,
                 // SAFETY: this is never used
                 std::ptr::null_mut(),
                 Some(log_hook),
             ));
-
-            // Signal managers that we want events ASAP
-            ffi!(instance.get_network_manager());
-            ffi!(instance.get_overlay_manager());
-            ffi!(instance.get_relationship_manager());
-            ffi!(instance.get_user_manager());
-
-            ffi!(instance.get_achievement_manager());
-            ffi!(instance.get_activity_manager());
-            ffi!(instance.get_lobby_manager());
-            ffi!(instance.get_store_manager());
-            ffi!(instance.get_voice_manager());
         }
+    }
 
-        Ok(instance)
+    #[allow(clippy::cognitive_complexity)]
+    fn kickstart_managers(&self) {
+        unsafe {
+            // Signal managers that we want events ASAP
+            ffi!(self.get_network_manager());
+            ffi!(self.get_overlay_manager());
+            ffi!(self.get_relationship_manager());
+            ffi!(self.get_user_manager());
+
+            ffi!(self.get_achievement_manager());
+            ffi!(self.get_activity_manager());
+            ffi!(self.get_lobby_manager());
+            ffi!(self.get_store_manager());
+            ffi!(self.get_voice_manager());
+        }
     }
 
     /// Runs all pending SDK callbacks.
@@ -115,12 +130,12 @@ impl Discord {
     /// [`Error::NotRunning`]: enum.Error.html#variant.NotRunning
     // We require &mut self to prevent calling during callbacks
     pub fn run_callbacks(&mut self) -> Result<()> {
-        unsafe { ffi!(self.run_callbacks()) }.to_result()
+        unsafe { ffi!(self.run_callbacks()).to_result() }
     }
 
     /// The Client ID that was supplied during creation
     pub fn client_id(&self) -> ClientID {
-        self.inner().client_id
+        self.0.client_id
     }
 
     /// Replaces the current event handler
@@ -128,19 +143,19 @@ impl Discord {
         &mut self,
         event_handler: Box<dyn EventHandler>,
     ) -> Option<Box<dyn EventHandler>> {
-        self.inner_mut().event_handler.replace(event_handler)
+        self.0.event_handler_mut().replace(event_handler)
     }
 
     /// Takes the current event handler, leaving `None` in its place
     pub fn take_event_handler(&mut self) -> Option<Box<dyn EventHandler>> {
-        self.inner_mut().event_handler.take()
+        self.0.event_handler_mut().take()
     }
 
     /// Returns some mutable reference to the event handler if it is of type T, or None if it isn't.
     // We require &mut self to prevent calling during callbacks
     pub fn downcast_event_handler<T: std::any::Any>(&mut self) -> Option<&mut T> {
-        self.inner_mut()
-            .event_handler
+        self.0
+            .event_handler_mut()
             .as_mut()
             .and_then(|e| e.downcast_mut())
     }
@@ -229,7 +244,10 @@ unsafe extern "C" fn log_hook(
 const ACHIEVEMENT: &sys::IDiscordAchievementEvents = &sys::IDiscordAchievementEvents {
     on_user_achievement_update: event_handler!(
         |user_achievement: *mut sys::DiscordUserAchievement| {
-            EventHandler::on_user_achievement_update(&*(user_achievement as *mut UserAchievement))
+            EventHandler::on_user_achievement_update(
+                // SAFETY: repr(transparent) allows this
+                &*(user_achievement as *mut UserAchievement),
+            )
         }
     ),
 };
@@ -244,7 +262,10 @@ const ACTIVITY: &sys::IDiscordActivityEvents = &sys::IDiscordActivityEvents {
     }),
 
     on_activity_join_request: event_handler!(|user: *mut sys::DiscordUser| {
-        EventHandler::on_activity_join_request(&*(user as *mut User))
+        EventHandler::on_activity_join_request(
+            // SAFETY: repr(transparent) allows this
+            &*(user as *mut User),
+        )
     }),
 
     on_activity_invite: event_handler!(
@@ -253,7 +274,9 @@ const ACTIVITY: &sys::IDiscordActivityEvents = &sys::IDiscordActivityEvents {
          activity: *mut sys::DiscordActivity| {
             EventHandler::on_activity_invite(
                 kind.into(),
+                // SAFETY: repr(transparent) allows this
                 &*(user as *mut User),
+                // SAFETY: repr(transparent) allows this
                 &*(activity as *mut Activity),
             )
         }
@@ -343,17 +366,26 @@ const RELATIONSHIP: &sys::IDiscordRelationshipEvents = &sys::IDiscordRelationshi
     on_refresh: event_handler!(|| EventHandler::on_relationships_refresh()),
 
     on_relationship_update: event_handler!(|relationship: *mut sys::DiscordRelationship| {
-        EventHandler::on_relationship_update(&*(relationship as *mut Relationship))
+        EventHandler::on_relationship_update(
+            // SAFETY: repr(transparent) allows this
+            &*(relationship as *mut Relationship),
+        )
     }),
 };
 
 const STORE: &sys::IDiscordStoreEvents = &sys::IDiscordStoreEvents {
     on_entitlement_create: event_handler!(|entitlement: *mut sys::DiscordEntitlement| {
-        EventHandler::on_entitlement_create(&*(entitlement as *mut Entitlement))
+        EventHandler::on_entitlement_create(
+            // SAFETY: repr(transparent) allows this
+            &*(entitlement as *mut Entitlement),
+        )
     }),
 
     on_entitlement_delete: event_handler!(|entitlement: *mut sys::DiscordEntitlement| {
-        EventHandler::on_entitlement_delete(&*(entitlement as *mut Entitlement))
+        EventHandler::on_entitlement_delete(
+            // SAFETY: repr(transparent) allows this
+            &*(entitlement as *mut Entitlement),
+        )
     }),
 };
 

src/utils.rs
@@ -1,12 +1,30 @@
 use crate::discord::{Discord, DiscordInner};
+use std::any::Any;
+
+// Thanks to Globi for the help
+// This is referred to as "upcasting"
+pub trait AsAny {
+    fn as_any(&self) -> &dyn Any;
+    fn as_any_mut(&mut self) -> &mut dyn Any;
+}
+
+impl<T: Any> AsAny for T {
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+
+    fn as_any_mut(&mut self) -> &mut dyn Any {
+        self
+    }
+}
 
 pub(crate) struct CallbackData<'d, T> {
-    pub(crate) discord: *const DiscordInner,
+    pub(crate) discord: *mut DiscordInner,
     pub(crate) callback: Box<dyn 'd + FnOnce(&Discord, T)>,
 }
 
-// Literal hack to be able to use `ffi!(...)` with `LobbyTransaction`, `LobbyMemberTransaction`,
-// and `SearchQuery`.
+// Literal hack to be able to use `ffi!(...)` with
+// `LobbyTransaction`, `LobbyMemberTransaction`, and `SearchQuery`.
 pub(crate) struct MacroHelper<T> {
     pub(crate) core: *mut T,
 }